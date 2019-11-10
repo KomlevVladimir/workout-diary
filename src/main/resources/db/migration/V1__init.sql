d4300fb0f8116db6c0eac5f9539314844b16e533
KomlevVladimir <roastr@gmail.com>
2019-11-10 21:06:24 +0300

    Creating user query was added

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -21,3 +21,6 @@ CREATE TABLE workouts (
   workout_date  DATE     NOT NULL,
   description   VARCHAR(4000) NOT NULL
 );
+
+INSERT INTO users (id, first_name, last_name, age, email, password, is_enabled) VALUES
+ (25, 'John', 'Doe', 23, 'test@myemail.com', '$2a$10$WpR3CCt8otTarm1xI8OMu.ip/edANp2JEdTWpqyM9uRhSXo5gpB0.', true);
